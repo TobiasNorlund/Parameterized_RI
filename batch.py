30edc20470800db5ce94eb8709a7dc4c664bad84
Tobias Norlund <tobno568@student.liu.se>
2016-01-19 16:34:15 +0100

    Modified batch.py to allow for replications

--- batch.py
+++ batch.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 import numpy as np
 
 # Which embeddings, models and datasets are available?
@@ -6,9 +7,15 @@ available_emb = ("BOW", "RI", "SGD_RI", "ATT_RI", "PMI_RI", "W2V", "PMI")
 available_mdl = ("MLP", "CNN")
 available_ds  = ("PL05", "SST")
 
-if len(sys.argv) != 4:
-    print "ERROR: Invalid number of arguments, format is: EMBEDDING1,EMBEDDING2,... MODEL1,MODEL2 DATASET(S)"
+replications = 1
+
+if len(sys.argv) != 4 and not(len(sys.argv) == 5 and str.isdigit(sys.argv[4])):
+    print "ERROR: Invalid number of arguments, format is: EMBEDDING1,EMBEDDING2,... MODEL1,MODEL2 DATASET(S) [replications=1]"
     sys.exit()
+
+# How many replications should we run?
+if len(sys.argv) == 5:
+    replications = int(sys.argv[4])
 
 embeddings = sys.argv[1]
 if embeddings == "-":
@@ -28,21 +35,25 @@ if dss == "-":
 else:
     dss = dss.split(",")   
 
-f_res = open("batch_results.txt", "w")
+f_res = open("batch_results_" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + ".txt", "w")
 results = []
 
 for emb in embeddings:
   for mdl in models:
     for ds in dss:
+      
       res = {}
       sys.argv = ["run.py", emb, mdl, ds]
+      aggr_acc = []
       try:
-          execfile("run.py", res)
-      
-          results.append((emb + " " + mdl + " " + ds, np.mean(res["accuracies"])*100, np.std(res["accuracies"])*100))
-
+          for rep in range(replications):  
+            execfile("run.py", res)
+            aggr_acc += res["accuracies"]
+          
+          results.append((emb + " " + mdl + " " + ds, aggr_acc))
+  
           # Write result to file
-          f_res.write("{}: {:.2f}% +- {:.2f}%\n".format(emb + " " + mdl + " " + ds, np.mean(res["accuracies"])*100, np.std(res["accuracies"])*100))
+          f_res.write("{}: {:.2f}% +- {:.2f}% \t{}\n".format(emb + " " + mdl + " " + ds, np.mean(aggr_acc*100), np.std(aggr_acc*100), aggr_acc))
           f_res.flush()
       except:
           print "Error running:" + emb + " " + mdl + " " + ds
@@ -54,7 +65,7 @@ print ""
 print " RESULTS:"
 print ""
 
-for experiment, acc, std in results:
+for experiment, aggr_acc in results:
 
-    print "{}: {:.2f}% +- {:.2f}%".format(experiment, acc, std)
+    print "{}: {:.2f}% +- {:.2f}%".format(experiment, np.mean(aggr_acc*100), np.std(aggr_acc*100))
       
